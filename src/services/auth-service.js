d80f2332348f2b15adeb568dfebec05abff9e9d2
bzwyant <bzwyant@gmail.com>
2023-04-23 17:19:31 -0400

    added ebay api

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,6 +1,15 @@
 import axios from "axios";
+import ebay from 'ebay-sdk-nodejs';
 const API_URL = "http://localhost:4000/api";
 const api = axios.create({ withCredentials: true });
+const ebayOptions = {
+  clientID: 'AntonioC-webdev-SBX-199285e91-6460598a',
+  clientSecret: 'SBX-99285e91c466-7050-4dc8-8a25-716e',
+  countryCode: 'US',
+  currencyCode: 'USD'
+};
+const ebayApi = ebayOptions.clientID ? new ebay(ebayOptions) : null;
+
 
 export const signup = async (email, username, password, role) => {
   const response = await api.post(`${API_URL}/signup`, {
