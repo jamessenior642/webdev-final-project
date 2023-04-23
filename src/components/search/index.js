16c4a9ef4b4dc600943d523278ac8aca2530e49c
Antonio Caceres <antonio.caceres178@gmail.com>
2023-04-23 19:07:15 -0400

    added ebay api

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { getProducts } from "../../services/product-service";
-import { Link } from "react-router-dom";
+import { getProductsbyKeyword2 } from "../../services/product-service";
+import { Link, useParams } from "react-router-dom";
 
 const Search = () => {
-  const [products, setProducts] = useState([]);
+    const { productSearch } = useParams();
+    const [products, setProducts] = useState([]);
+
+
   const productList = async () => {
-    const response = await getProducts();
-    setProducts(response.data);
+    const keyword =  productSearch;
+    const response = await getProductsbyKeyword2(productSearch);
+    setProducts(response.itemSummaries);
   };
 
   useEffect(() => {
@@ -18,21 +22,21 @@ const Search = () => {
       <h1 className="text-center my-4">Search Results</h1>
       <div className="row"> 
         {products.map((product) => (
-          <div className="col-md-4 mb-4" key={product.id}>
+          <div className="col-md-4 mb-4" key={product.itemId}>
             <div className="card h-100"> 
               <img
-                src={"img from api"}
+                src={product.image.imageUrl}
                 alt="product"
                 className="card-img-top"
                 height="200"
                 style={{ objectFit: "cover" }}
               />
               <div className="card-body">
-                <Link className = "text-decoration-none" to={`/details/${product.id}`}>
-                  <h3 className="card-title">title from api</h3>
+                <Link className = "text-decoration-none" to={`/details/${product.itemId}`}>
+                  <h3 className="card-title">{product.title}</h3>
                 </Link>
-                <p className="card-text">description from api</p>
-                <p className="card-text">$price from api</p>
+                <p className="card-text">Condition: {product.condition}</p>
+                <p className="card-text">${product.price.value}</p>
               </div>
             </div>
           </div>
