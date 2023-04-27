64dd3a0a3844db70ecd271a8fd642dd51d55a9c6
bzwyant <bzwyant@gmail.com>
2023-04-27 15:25:06 -0400

    reviewed products show on profile

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import * as service from "../../services/auth-service";
 import { useProfile } from "../../context/profile-context";
 import * as userService from "../../services/user-service";
 import { Link } from "react-router-dom";
+import * as productService from "../../services/product-service";
+import * as reviewService from "../../services/review-service";
 
 const Profile = () => {
   const { profile, loading } = useProfile();
@@ -10,7 +12,28 @@ const Profile = () => {
   const [email, setEmail] = React.useState();
   const [password, setPassword] = React.useState();
   const [err, setError] = React.useState(false);
-
+  
+  const userID = profile._id;
+  const [reviews, setReviews] = useState([]);
+  const [products, setProducts] = useState([]);
+  
+  const fetchProducts = async () => {
+    // wait for reviews to be fetched
+    const reviews = await fetchReviews();
+    setReviews(reviews);
+    
+    // for every review in reviews, create a list of products
+    const products = await Promise.all(reviews.map(async (r) => {
+      const product = await productService.getProductById2(r.productID);
+      return product;
+    }));
+    
+    setProducts(products);
+  }
+  
+  const fetchReviews = async () => {
+    return reviewService.findReviewsByUserId(userID)
+  }
 
   const checkUser = async () => {
     const user = await userService.getUserByEmail(profile.email);
@@ -21,6 +44,7 @@ const Profile = () => {
 
   React.useEffect(() => {
     checkUser();
+    fetchProducts()
   }, []);
 
   return (
@@ -85,6 +109,32 @@ const Profile = () => {
                   Please make sure you have a valid login!
                 </div>
               )}
+            </div>
+          </div>
+          <div className="reviews my-5">
+            <h2>Reviewed Products</h2>
+            <div className="row">
+              {products.map((product) => (
+                  <div className="col-md-4 mb-4" key={product.itemID}>
+                    <div className="card h-100">
+                      <img
+                          src={product.image.imageUrl}
+                          alt="product"
+                          className="card-img-top"
+                          height="200"
+                          style={{objectFit: "cover"}}
+                      />
+                      <div className="card-body">
+                        <Link className="text-decoration-none" to={`/details/${product.itemId}`}>
+                          <h3 className="card-title">{product.title}</h3>
+                        </Link>
+                        <p className="card-text">Condition: {product.condition}</p>
+                        <p className="card-text">${product.price.value}</p>
+                        <p className="card-text">Review: {reviews[products.indexOf(product)].text}</p>
+                      </div>
+                    </div>
+                  </div>
+              ))}
             </div>
           </div>
         </div>
